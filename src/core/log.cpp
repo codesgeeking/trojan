63ff53f3200cf15fe41d2caa7e40c94a9ffffeb6
codingdie <codingdie@gmail.com>
2020-11-12 01:28:15 +0800

    update

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -18,15 +18,15 @@
  */
 
 #include "log.h"
-#include <cstring>
-#include <cerrno>
-#include <stdexcept>
-#include <sstream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/posix_time/posix_time_io.hpp>
+#include <cerrno>
+#include <cstring>
+#include <sstream>
+#include <stdexcept>
 #ifdef ENABLE_ANDROID_LOG
 #include <android/log.h>
-#endif // ENABLE_ANDROID_LOG
+#endif// ENABLE_ANDROID_LOG
 using namespace std;
 using namespace boost::posix_time;
 using namespace boost::asio::ip;
@@ -44,7 +44,7 @@ void Log::log(const string &message, Level level) {
 #else
         fprintf(output_stream, "%s\n", message.c_str());
         fflush(output_stream);
-#endif // ENABLE_ANDROID_LOG
+#endif// ENABLE_ANDROID_LOG
         if (log_callback) {
             log_callback(message, level);
         }
@@ -52,13 +52,14 @@ void Log::log(const string &message, Level level) {
 }
 
 void Log::log_with_date_time(const string &message, Level level) {
-    static const char *level_strings[]= {"ALL", "INFO", "WARN", "ERROR", "FATAL", "OFF"};
+    static const char *level_strings[] = {"ALL", "INFO", "WARN", "ERROR", "FATAL", "OFF"};
     auto *facet = new time_facet("[%Y-%m-%d %H:%M:%S] ");
     ostringstream stream;
     stream.imbue(locale(stream.getloc(), facet));
     stream << second_clock::local_time();
     string level_string = '[' + string(level_strings[level]) + "] ";
     log(stream.str() + level_string + message, level);
+    delete facet;
 }
 
 void Log::log_with_endpoint(const tcp::endpoint &endpoint, const string &message, Level level) {
